36fa8210cd1b1abf73d8629a87dafd81e1f9b92c
KBrandtjen <Keithbrandtjen09@gmail.com>
2016-08-16 15:34:03 -0700

    all tasks done

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,7 +1,7 @@
 var articles = [];
 
 function Article (opts) {
-  // TODO: Use the js object passed in to complete this constructor function:
+  // TODO: DONE Use the js object passed in to complete this constructor function:
   // Save ALL the properties of `opts` into `this`
   this.author = opts.author;
   this.authorUrl = opts.authorUrl;
@@ -13,9 +13,14 @@ function Article (opts) {
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
+  $newArticle.find('h1').text(this.title);
   $newArticle.attr('data-category', this.category);
+  $newArticle.find('.byline a').text(this.author);
+  $newArticle.find('.byline a').attr('href', this.authorUrl);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.find('time').attr('datetime', this.publishedOn);
 
-  /* TODO: Now use jQuery to fill in the rest of the current
+  /* TODO: DONE Now use jQuery to fill in the rest of the current
   template clone with properties from this particular Article instance.
   We need to fill in:
     1. author name,
@@ -24,16 +29,16 @@ Article.prototype.toHtml = function() {
     4. article body, and
     5. publication date. */
     // Display the date as a relative number of 'days ago'
-    $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-    $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
 
-    /* TODO: This cloned article is no longer a template,
+    /* TODO: DONE This cloned article is no longer a template,
     as it now has real data attached to it! We need to account
     for that before this current article gets rendered to our
     DOM. */
 
-    $('article.template').removeClass('template');
-    return $newArticle;
+  $newArticle.removeClass();
+  return $newArticle;
 };
 
 ourLocalData.sort(function(firstElement, secondElement) {
